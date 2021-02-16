55415d72f3c804bbeb091685342f3c889446c23c
Katacoda Scenario <scenario@katacoda.com>
2021-02-16 20:58:19 +0000

    Solve test-lab
    Signed-off-by: Katacoda Scenario <scenario@katacoda.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -1,9 +1,26 @@
+/*Julio Rosales */
 package main
+
 
 import (
 	"fmt"
+	"os"
 )
 
+
 func main() {
-	fmt.Println("Welcome to the jungle")
+	if(1==len(os.Args)){
+	  fmt.Println("Error, a name must be provided!  ")
+	}else if(2<=len(os.Args)){
+	  fmt.Print("Hello ")
+	  for i :=1 ; i < len(os.Args); i++ {
+	      if(i==len(os.Args)-1){
+	       fmt.Print(os.Args[i]+", ") 
+	      }else{
+	        fmt.Print(os.Args[i]+" ") 
+	      }
+	  }
+	  fmt.Print("Welcome to the jungle.")
+	  fmt.Println("")
+	}
 }
