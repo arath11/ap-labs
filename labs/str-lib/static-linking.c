f84a8fa7f8c084c0e6d65866518d1a62d342e434
arath11 <julioarath@gmail.com>
2021-03-24 01:06:32 +0000

    Solve str-lib
    Signed-off-by: arath11 <julioarath@gmail.com>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <stdbool.h>
 
-int main() {
-    return 0;
+
+extern int mystrlen(char *str);
+extern char *mystradd(char *origin, char *addition);
+extern int mystrfind(char *origin, char *substr);
+
+
+int main(int argc, char **argv) {
+    
+    if(strcmp(argv[1],"-add")==0 && argc==4){
+
+        printf("\nInitial Lenght      : %d\n", mystrlen(argv[2]));
+        char *newString = mystradd(argv[2], argv[3]);
+        printf("New String          : %s\n", newString );
+        printf("New String          : %d\n", mystrlen(newString));
+
+
+        return 0;
+    }else if(strcmp(argv[1],"-find")==0 && argc==4){
+
+
+        int pos = mystrfind(argv[2], argv[3]);
+        printf("\n[%s] string was found at [%d] position\n", argv[3], pos);
+
+    }else{    
+        printf("\nPlease enter valid comands\n\n");
+        return 0;
+    }
+
+
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,43 @@
 
 
 int mystrlen(char *str){
-    return 0;
+    int tamano=0;
+    char *c;
+
+    for(c=str;*c != '\0';c++){
+        tamano++;
+    }
+    
+    return tamano;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *añadido=origin+mystrlen(origin);
+
+    while(*addition != '\0'){
+        *añadido++= *addition++;
+
+    }
+    
+    *añadido='\0';
+    return origin;
+
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int onta = 0,
+        PosOriginal = 0,
+        i = 0,
+        tamaño = mystrlen(substr);
+    while(origin[PosOriginal] != '\0') {
+        onta = PosOriginal;
+        while(origin[PosOriginal] != '\0' && origin[PosOriginal]==substr[i] ){
+            i++;
+            PosOriginal++;
+        }
+        if(i == tamaño){return onta;}
+        i = 0;
+        PosOriginal++;   
+    }
+    return -1;
 }
