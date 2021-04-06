d9c18e017e8e3adae81b32af5c36c0e3e95693f4
arath11 <julioarath@gmail.com>
2021-04-06 19:56:48 +0000

    Solve go-functions-methods
    Signed-off-by: arath11 <julioarath@gmail.com>

--- labs/go-functions-methods/geometry.go
+++ labs/go-functions-methods/geometry.go
@@ -1,34 +1,34 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 156.
-
-// Package geometry defines simple types for plane geometry.
-//!+point
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
 
-type Point struct{ X, Y float64 }
+type Point struct{ x, y float64 }
 
-// traditional function
+func (p Point) X() float64 {
+	return p.x
+}
+
+func (p Point) Y() float64 {
+	return p.y
+}
+
 func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
 }
 
-// same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Round(math.Hypot(q.X()-p.X(), q.Y()-p.Y())*100) / 100
 }
 
-//!-point
-
-//!+path
-
-// A Path is a journey connecting the points with straight lines.
 type Path []Point
 
-// Distance returns the distance traveled along the path.
 func (path Path) Distance() float64 {
 	sum := 0.0
 	for i := range path {
@@ -39,7 +39,115 @@ func (path Path) Distance() float64 {
 	return sum
 }
 
-func main() {
+func onSegment(a, b, c Point) bool {
+	return (b.x <= math.Max(a.x, c.x) && b.x >= math.Min(a.x, c.x) &&
+		b.y <= math.Max(a.y, c.y) && b.y >= math.Min(a.y, c.y))
 }
 
-//!-path
+func orientation(a, b, c Point) int {
+	var val float64 = (b.y-a.y)*(c.x-b.x) - (b.x-a.x)*(c.y-b.y)
+
+	if val == 0 {
+		return 0 // colinear
+	}
+	if val < 0 {
+		return 2 //counter clockwise
+	} else {
+		return 1 // clockvise
+	}
+}
+
+//return true if they intersect
+func doIntersect(p1, q1, p2, q2 Point) bool {
+
+	o1 := orientation(p1, q1, p2)
+	o2 := orientation(p1, q1, q2)
+	o3 := orientation(p2, q2, p1)
+	o4 := orientation(p2, q2, q1)
+
+	//caso general
+
+	if (o1 != o2) && (o3 != o4) {
+		return true
+	}
+
+	//colineales
+	if (o1 == 0) && (onSegment(p1, p2, q1)) {
+		return true
+	} else if (o2 == 0) && (onSegment(p1, q2, q1)) {
+		return true
+	} else if (o3 == 0) && (onSegment(p2, p1, q2)) {
+		return true
+	} else if (o4 == 0) && (onSegment(p2, q1, q2)) {
+		return true
+	}
+
+	return false
+}
+
+func (tmp Path) verificar() bool {
+	var seIntersectan bool
+
+	for i := 0; i < len(tmp)-3 && !seIntersectan; i++ {
+
+		seIntersectan = doIntersect(tmp[i], tmp[i+1], tmp[i+2], tmp[i+3])
+
+	}
+
+	return seIntersectan
+}
+
+func main() {
+	path1 := Path{}
+
+	if len(os.Args) == 2 {
+		if sides, err := strconv.Atoi(os.Args[1]); err == nil {
+			if sides > 2 {
+
+				fmt.Printf("- Generating a [%d] sides figure\n", sides)
+				for i := 0; i < sides; i++ {
+					inicioRandom := rand.NewSource(time.Now().UnixNano())
+					random := rand.New(inicioRandom)
+
+					path1 = append(path1, Point{random.Float64()*100 - (-100) + (-100), random.Float64()*100 - (-100) + (-100)})
+
+					//verificar
+					for path1.verificar() {
+						//hara uno nuevo si encuentra problemas
+						path1[i] = Point{random.Float64()*100 - (-100) + (-100), random.Float64()*100 - (-100) + (-100)}
+					}
+				}
+
+				//imprimir
+
+				fmt.Printf("- Figure's vertices\n")
+				for i := range path1 {
+					fmt.Printf("   ( %.2f, %.2f)\n", path1[i].X(), path1[i].Y())
+				}
+
+				fmt.Println("- Figure's Perimeter")
+				fmt.Printf("- ")
+
+				for i := 0; i < sides; i++ {
+					if i == 0 {
+						fmt.Printf("   %.2f+", path1[i].Distance(path1[(i+1)%sides]))
+					} else {
+						fmt.Printf("  %.2f  +", path1[i].Distance(path1[(i+1)%sides]))
+					}
+				}
+				fmt.Printf("%.2f  ", path1[len(path1)-2].Distance(path1[len(path1)-1]))
+				//fmt.Printf("%.2f = %.2f\n", path[len(path)-2].Distance(path[len(path)-1]), path.Distance())
+
+				//prueba2
+				fmt.Printf(" = %.2f\n", path1.Distance()+path1[sides-1].Distance(path1[0]))
+
+			} else {
+				fmt.Printf("No se puede generar, necesita más lados \n")
+			}
+		} else {
+			fmt.Printf("Error\n")
+		}
+	} else {
+		fmt.Printf("Error\n")
+	}
+}
