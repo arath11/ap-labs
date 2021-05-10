b9fe68c810bab972c587f144b9c2a55690c9b038
arath11 <julioarath@gmail.com>
2021-05-10 00:45:34 +0000

    Solved second partial
    Signed-off-by: arath11 <julioarath@gmail.com>

--- challenges/second-partial/chat/client.go
+++ challenges/second-partial/chat/client.go
@@ -34,7 +34,7 @@ func main() {
 
 	//entrada
 	//go run client.go -user user1 -server localhost:9000
-	//s
+	//s1
 	if args[3] == "-server" && args[1] == "-user" {
 		servidor = args[4]
 		usuario = args[2]
